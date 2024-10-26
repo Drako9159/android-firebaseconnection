ead37f2e1459fbe35930bfaee99ab880e8828859
drako9159 <chochito9159@gmail.com>
2024-10-25 18:15:53 -0700

    feat(screen): configure initial screen

--- app/src/main/java/com/niojar/firebaseconnecion/presentation/initial/InitialScreen.kt
+++ app/src/main/java/com/niojar/firebaseconnecion/presentation/initial/InitialScreen.kt
@@ -1,8 +1,67 @@
 package com.niojar.firebaseconnecion.presentation.initial
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.niojar.firebaseconnecion.R
+import com.niojar.firebaseconnecion.ui.theme.Black
+import com.niojar.firebaseconnecion.ui.theme.Gray
+import com.niojar.firebaseconnecion.ui.theme.Green
 
+@Preview
 @Composable
-fun InitialScreen(){
-
+fun InitialScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Brush.verticalGradient(listOf(Gray, Black), startY = 0f, endY = 600f)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.weight(1f))
+        Image(
+            painter = painterResource(id = R.drawable.spotify),
+            contentDescription = "Spotify Logo",
+            modifier = Modifier.clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Text(
+            "Millions of songs.",
+            color = Color.White,
+            fontSize = 38.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            "Free on Spotify",
+            color = Color.White,
+            fontSize = 38.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Button(
+            onClick = {}, modifier = Modifier
+                .fillMaxWidth()
+                .padding(32.dp), colors = ButtonDefaults.buttonColors(containerColor = Green)
+        ) {
+            Text(text = "Sign up free", color = Black)
+        }
+    }
 }
