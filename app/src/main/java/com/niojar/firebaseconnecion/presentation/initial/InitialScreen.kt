ef8a27a5e2d55b0ea6ee8bdc0adedb573448160d
drako9159 <chochito9159@gmail.com>
2024-10-25 18:41:55 -0700

    fea(screen): finish ui initial screen

--- app/src/main/java/com/niojar/firebaseconnecion/presentation/initial/InitialScreen.kt
+++ app/src/main/java/com/niojar/firebaseconnecion/presentation/initial/InitialScreen.kt
@@ -2,11 +2,16 @@ package com.niojar.firebaseconnecion.presentation.initial
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -17,15 +22,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.niojar.firebaseconnecion.R
+import com.niojar.firebaseconnecion.ui.theme.BackgroundButton
 import com.niojar.firebaseconnecion.ui.theme.Black
 import com.niojar.firebaseconnecion.ui.theme.Gray
 import com.niojar.firebaseconnecion.ui.theme.Green
+import com.niojar.firebaseconnecion.ui.theme.ShapeButton
 
 @Preview
 @Composable
@@ -50,18 +59,69 @@ fun InitialScreen() {
             fontWeight = FontWeight.Bold
         )
         Text(
-            "Free on Spotify",
+            "Free on Spotify.",
             color = Color.White,
             fontSize = 38.sp,
             fontWeight = FontWeight.Bold
         )
         Spacer(modifier = Modifier.weight(1f))
         Button(
-            onClick = {}, modifier = Modifier
+            onClick = {},
+            modifier = Modifier
                 .fillMaxWidth()
-                .padding(32.dp), colors = ButtonDefaults.buttonColors(containerColor = Green)
+                .height(48.dp)
+                .padding(horizontal = 32.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Green)
         ) {
-            Text(text = "Sign up free", color = Black)
+            Text(text = "Sign up free", color = Black, fontWeight = FontWeight.Bold)
         }
+        Spacer(modifier = Modifier.height(8.dp))
+        CustomButtons(
+            Modifier.clickable { },
+            painterResource(id = R.drawable.google),
+            "Continue with Google"
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        CustomButtons(
+            Modifier.clickable { },
+            painterResource(id = R.drawable.facebook),
+            "Continue with Facebook"
+        )
+        Text(
+            text = "Log In",
+            color = Color.White,
+            modifier = Modifier.padding(24.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.weight(1f))
+    }
+}
+
+@Composable
+fun CustomButtons(modifier: Modifier, painter: Painter, title: String) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(48.dp)
+            .padding(horizontal = 32.dp)
+            .background(BackgroundButton)
+            .border(2.dp, ShapeButton, CircleShape),
+        contentAlignment = Alignment.CenterStart
+
+    ) {
+        Image(
+            painter = painter,
+            contentDescription = "$title Logo",
+            modifier = Modifier
+                .padding(start = 16.dp)
+                .size(16.dp)
+        )
+        Text(
+            text = title,
+            color = Color.White,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
